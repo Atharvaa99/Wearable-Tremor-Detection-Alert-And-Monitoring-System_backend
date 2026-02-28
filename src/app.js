8f5a7830c649b0cb9653c89208e57003fdab73ac
Atharvaa99 <atharvadhaigude@icloud.com>
2026-02-28 08:44:19 +0530

    CORS added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const authRoute = require('./routes/auth.routes');
 const deviceRoute = require('./routes/device.routes');
 const patientRoute = require('./routes/patient.route');
 
 const app = express();
 
+app.use(cors({
+    origin: '*', // during development
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key']
+}));
+
 app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/device', deviceRoute);
 app.use('/api/patient', patientRoute);
-
-
-module.exports = app;
